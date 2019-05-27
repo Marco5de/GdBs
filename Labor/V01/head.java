62e4fbe1e9d22f0a8941a3ed7472daf24d5cf712
marco5de <marco.deuscher@t-online.de>
2019-05-27 20:58:51 +0200

    java stuff for V01. pipes don't work

---
+++ Labor/V01/head.java
@@ -0,0 +1,112 @@
+import static cTools.KernelWrapper.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+//Todo complete help printout
+//ToDo if FIlename = "-" read from stdin
+class head{
+
+	public static void main(String[] args){
+		int nbytes = 0;
+		int nlines = 10;	
+
+		boolean param_used = false;
+
+		//check if there are enough input args supplied
+		if(args.length<1){
+			System.out.println("Not enough args");
+			exit(0);
+		}
+
+		if(args[0].charAt(0) == '-' && args[0].length()>1){
+			switch(args[0].charAt(1)){
+				case 'c':
+					nlines = Integer.parseInt(args[1]);
+					if(nlines<0){System.err.println("Neg. number of lines");exit(1);}
+					param_used = true;
+					break;
+				case 'n':
+					nbytes = Integer.parseInt(args[1]);
+					if(nbytes<0){System.err.println("Neg. number of bytes");exit(1);}
+					param_used = true;
+					break;	
+				case '-':
+					if(args[0].contains("help")){
+						System.out.println("ToDo add help in here");
+						exit(0);
+					}break;	
+			}
+		}
+		ArrayList<String> list = new ArrayList<>(Arrays.asList(args));
+
+		if(param_used){
+			//remove entries that arent files
+			list.remove(0);
+			list.remove(0);
+		}
+		
+		//open one file at a time, read the specified amount and print it
+		boolean header = list.size()>1;
+		for(String s : list){
+			if(header)
+				System.out.println("===> " + s + " <===");
+			int fd;
+			if(s.equals("-")){
+				fd = STDIN_FILENO;
+			}else{
+				if((fd = open(s,O_RDONLY))<0){
+					System.err.println("Error opening File");
+					exit(1);
+				}
+			}
+			if(nbytes == 0){
+				read_and_println(fd,nlines);
+			}else{
+				read_printbytes(fd,nbytes);
+			}
+		}
+		exit(0);
+		
+	}
+	private static int read_and_println(int fd,int nlines){
+		byte[] buffer = new byte[1];
+		
+		int res = 0;
+		int len;
+		int line_count = 0;
+		String out = "";
+
+		while(line_count < nlines){
+			if((res = read(fd,buffer,1))<0)
+				return -1;
+		
+			if(buffer[0]=='\n'){
+				line_count++;	
+			}	
+			if(res == 0)
+				break;
+			out += (char)buffer[0];
+		}
+		System.out.println(out);
+		return 0;
+	}
+
+	private static int read_printbytes(int fd, int nbytes){
+		byte[] buffer = new byte[256];
+
+		int total = 0;
+		int res;
+		String out = "";
+
+		while(total < nbytes){
+			if((res=read(fd,buffer,nbytes-total))<0)
+				return -1;
+			total += res;
+			if(res==0)break;
+			out += new String(buffer);
+		}
+		System.out.println(out);
+
+		return 0;
+	}
+}
+
---
+++ Labor/V01/shell.java
@@ -0,0 +1,197 @@
+import static cTools.KernelWrapper.*;
+import java.util.Scanner;
+import java.io.File;
+import java.util.Arrays;
+
+//create_proc muss von anfang an ein spez. stdin nehmen, damit proc nach der pip arbeiten kann
+//zusammenspiel von vorherigen Prozess und prozess in den gepiped wird, muss noch irgendwie schöner gemacht werden
+
+
+class shell {
+  public static void main(String[] args) {
+	  
+	String env = System.getenv("PATH");
+	String[] path = env.split(":");
+	
+	 while(true){
+		String[][][] input = splitInput();
+		int pipe_count = 0;	
+
+
+		pipe_count = input.length-1;
+		for(String[][] pipes : input){
+
+			int[] pip_fd = new int[2];
+			if(pipe_count > 0){
+				if(pipe(pip_fd)<0)
+					System.err.println("Pipe failed");
+				pipe_count--;
+			}
+			for(String[] comm : pipes){	
+				return_valid_path(comm,path);	
+				String[][] files = parse_redirect(comm);
+					
+				if(create_proc(files[0],files[1][0],files[2][0],pip_fd) != 0){
+					break;
+				}
+			}
+		}	
+	}
+
+  }	//return String[][] (param,fin,fout)
+	static String[][] parse_redirect(String[] arr){
+		int i;
+		int index_out=0;
+		int index_in=0;
+		for(i=0; i<arr.length; i++){
+			if(arr[i].equals(">"))
+				index_out = i;
+			else if(arr[i].equals("<"))
+				index_in = i;
+		}
+		String[][] ret = new String[3][i];
+		ret[1][0] = null;
+		ret[2][0] = null;
+
+		if(index_out!=0){
+			ret[2][0] = arr[index_out+1];
+		}
+		if(index_in!=0){
+			ret[1][0] = arr[index_in+1];	
+		}
+
+		if(index_out > index_in){
+			if(index_in != 0)
+				ret[0] = Arrays.copyOfRange(arr,0,index_in);
+			else
+				ret[0] = Arrays.copyOfRange(arr,0,index_out);
+		}else if(index_out < index_in){
+			if(index_out != 0)
+				ret[0] = Arrays.copyOfRange(arr,0,index_out);
+			else
+				ret[0] = Arrays.copyOfRange(arr,0,index_in);
+		}else 
+			ret[0] = arr;
+		
+		return ret;
+	}
+  	static int redirect_output(String fout,String fin){
+		//case redirect stdout
+		if(fout != null){
+			//close stdout and open file in arr[i+1]
+			close(STDOUT_FILENO); // 1 = stdout
+			int fd;
+			if((fd=open(fout,O_WRONLY | O_CREAT))<0)
+				return -1;
+		}
+		if(fin != null){
+			close(STDIN_FILENO);
+			int fd;
+			if((fd=open(fin,O_RDONLY))<0)
+				return -1;
+		}
+		return 0;
+	}	
+
+  	static void return_valid_path(String[] arr,String[] env){
+		for(String s : env){
+			File tmp = new File(s +"/"+ arr[0]);
+			if(tmp.exists()){
+				arr[0] = s +"/"+ arr[0];
+				break;
+			}
+		}
+	}
+
+	static String[][][] splitInput() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Custom Shell: $ ");
+		String input = in.nextLine();
+
+
+
+		String[] pip_sep = input.split(" \\| ");
+
+
+		String[][] sep_input = new String[pip_sep.length][]; 
+		for(int i = 0; i<pip_sep.length; i++){
+			sep_input[i] = pip_sep[i].split(" && ");
+		}
+		String[][][] split_input = new String[sep_input.length][][];  
+		for(int i=0; i< sep_input.length; i++){
+			split_input[i] = new String[sep_input[i].length][];
+			for(int j=0; j<sep_input[i].length; j++) {
+				split_input[i][j] = sep_input[i][j].split(" ");	
+			}
+		}
+		
+		for(String[][] d2 : split_input){
+			for(String[] d1 : d2){
+				System.err.println(Arrays.toString(d1));
+			}
+		}
+		
+		return split_input;
+	}
+
+	static int create_proc(String[] split_input,String fin, String fout,int[] pip_fd){
+		if(split_input[0].equals("exit")){
+			System.err.println("\n");	
+			exit(0);
+		}
+
+		int[] status = new int[1];
+		int pid_child;
+
+
+		if((pid_child = fork())<0){
+			System.err.println("Error: fork");
+			exit(1);
+		}
+
+		//unterscheidung zw. child und parent
+		if(pid_child == 0){
+			//code for child
+			if(fout != null || fin != null){
+				if(redirect_output(fout,fin)!=0){
+					System.err.println("Error redirecting output");
+					return 1;
+				}
+			}
+			System.err.println("Pip Filed: " + pip_fd[0]+ "  "  + pip_fd[1]);
+			int result = handle_pipes(pip_fd);
+			//code for child goes in here
+			if(execv(split_input[0],split_input)<0){
+				System.err.println("Error: execv");
+				exit(1);
+			}
+		}
+		else{
+			//code for parent
+			if(waitpid(pid_child,status,0)<0){
+				System.err.println("Error: waiting for child");
+				exit(1);
+			}
+			if(status[0] != 0){
+				System.err.println("Child returned error code");
+				return 1;
+			}
+		}	
+	
+		return 0;
+	}
+
+	static int handle_pipes(int[] fd){
+		if(fd[0]==0 && fd[1]==0)
+			return -1;
+
+		//fd[0] ist für das Lesen verantwortlich. Wird im folgenden nicht verwendet und daher geschlossen
+		close(fd[0]);
+
+		//es muss jetzt noch das Out des jetzigen Prozesses neu gesetzt werden
+		dup2(fd[0],1);
+		close(fd[0]);dd
+		return 0;
+		
+	}
+}
